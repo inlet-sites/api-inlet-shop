b83fa0bb037abd2349c6e4b074b15c5abfb99a58
Lee Morgan <lee@leemorgan.dev>
2024-10-16 10:34:43 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import compression from "compression";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+let mongoString = "mongodb://127.0.0.1/inletshop";
+if(process.env.NODE_ENV === "production"){
+    mongoString = `mongodb://website:${process.env.MONGODB_PASS}@127.0.0.1:27017/inletsites?authSource=admin`;
+}
+mongoose.connect(mongoString);
+
+app.use(compression());
+app.use(express.json());
+app.use(cors());
+
+if(process.env.NODE_ENV === "production"){
+    export default app;
+}else{
+    app.listen(8000);
+}
