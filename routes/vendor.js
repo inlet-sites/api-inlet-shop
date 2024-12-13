c60b8c96d072ee8d58733fd11d3f34a8fd1c7c33
Lee Morgan <lee@leemorgan.dev>
2024-12-13 11:23:48 -0500

    Remove dead code from vendor routes.

--- routes/vendor.js
+++ routes/vendor.js
@@ -1,8 +1,4 @@
-import Vendor from "../models/vendor.js";
-
-import {httpError} from "../error.js";
 import {vendorAuth} from "../auth.js";
-import sendEmail from "../sendEmail.js";
 import {
     createPassRoute,
     changePassRoute,
@@ -14,38 +10,9 @@ import {
     changeImageRoute,
     passwordEmailRoute,
     resetPasswordRoute,
-
-    confirmToken,
-    passwordLength,
-    passwordMatch,
-    createPasswordHash,
-    newToken,
-    validPassword,
-    createToken,
-    createImage,
-    removeImage,
-    updateVendor,
-    responseVendor
 } from "../controllers/vendor.js";
-import resetPasswordEmail from "../email/resetPassword.js";
 
 const vendorRoutes = (app)=>{
-    const getVendor = async (res, id)=>{
-        let vendor;
-        try{
-            vendor = await Vendor.findOne({_id: id});
-        }catch(e){
-            console.error(e);
-            httpError(res, 500, "Internal server error (err-001)");
-            return null;
-        }
-        if(!vendor){
-            httpError(res, 400, "Vendor with this ID doesn't exist");
-            return null;
-        }
-        return vendor
-    }
-
     app.put("/vendor/:vendorId/password/:token", createPassRoute);
     app.put("/vendor/:vendorId/password", vendorAuth, changePassRoute)
     app.post("/vendor/token", getTokenRoute);
