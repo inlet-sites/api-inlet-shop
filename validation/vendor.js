359505bc691b836c795616ed835a69daed235660
Lee Morgan <lee@leemorgan.dev>
2024-12-13 08:33:31 -0500

    Create validation for vendor password.

--- validation/vendor.js
+++ validation/vendor.js
@@ -1,3 +1,5 @@
+import {CustomError} from "../CustomError.js";
+
 /*
  Validate Data for a vendor
  Returns error if there is any invalid data
@@ -6,5 +8,13 @@
 
  */
 export default (data)=>{
-    return null;
+    if(data.password){
+        if(typeof(data.password) !== "string") throw new CustomError(400, "Invalid password");
+        if(data.password.length < 10){
+            throw new CustomError(400, "Password must contain at least 10 characters");
+        }
+        if(data.confirmPassword && data.password !== data.confirmPassword){
+            throw new CustomError(400, "Passwords do not match");
+        }
+    }
 }
