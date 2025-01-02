8165177ca06eebdb6251463201db8e0eb04b035f
Lee Morgan <lee@leemorgan.dev>
2025-01-02 16:37:15 -0500

    Bug fix: route order didn't work.

--- routes/order.js
+++ routes/order.js
@@ -13,7 +13,7 @@ export default (app)=>{
     app.post("/order", createRoute);
     app.post("/order/webhook/:vendorId", webhookRoute);
     app.get("/order/:orderId/token/:token", getOrderRoute);
-    app.get("/order?*", vendorAuth, getOrdersRoute);
     app.get("/order/:orderId/vendor", vendorAuth, getOrderVendorRoute);
+    app.get("/order?*", vendorAuth, getOrdersRoute);
     app.put("/order/:orderId", vendorAuth, updateOrderRoute);
 }
